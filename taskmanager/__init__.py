8a88b045f2268f8f2c523ea79eec9c9f3b28b64e
Hawraa Hijazi <hawraahijazi1996@gmail.com>
2025-02-08 11:01:23 +0000

    project not deployed

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -6,8 +6,14 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///site.db")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 
 db = SQLAlchemy(app)
