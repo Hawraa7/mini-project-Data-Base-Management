f3d045ca36585c96b8fcfa9bdaaa424eef320971
Hawraa7 <hawraahijazi1996@gmail.com>
2025-02-15 02:40:12 -1000

    Update __init__.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -8,7 +8,7 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL", 'postgresql://neondb_owner:npg_q8iGc4HrQCXj@ep-steep-dust-a2sen6ew.eu-central-1.aws.neon.tech/patch_lash_truce_629825')
 else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
